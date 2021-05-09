4a6ffcc05253d34d29f76e0df19dc346249de81c
WuZhen-liang <a0978262695@gmail.com>
2021-05-09 12:03:03 +0800

    My practice

---
+++ TestProject/src/TestBasic.java
@@ -0,0 +1,19 @@
+import java.math.*;
+
+public class TestBasic {
+
+	public static void main(String[] args) {
+		
+		short k = 10;
+		int i = k + k;
+		System.out.println("i="+i);
+		int comeOn = 10;
+		int j = 2100000000;
+		int l = j + j;
+		System.out.println(l);//資料溢位
+		BigDecimal bd = new BigDecimal("10.123");
+		BigDecimal bd1 = new BigDecimal(10.123);
+		System.out.println(bd+"/n"+bd1);
+	}
+
+}
