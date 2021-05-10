52a40b897add1361d8396a2d68bcb90cdf860f39
WuZhen-liang <a0978262695@gmail.com>
2021-05-10 11:18:11 +0800

    TestPackage Note updata

--- JavaLab/src/TestPackage.java
+++ JavaLab/src/TestPackage.java
@@ -12,6 +12,21 @@ public class TestPackage {
 		/*		若上面宣告import category.Item 下方只需要打Item 	
 		Item  item = new Item();
 		item.name = "Asus notebook"; */
+/*						
+	cmd執行--->C:\iii\java\javaWorkspace\JavaLab\bin java category.Item  -->O % 一定要在主目錄bin 
+    				return:Item's main 方法  
+	cmd執行--->C:\iii\java\javaWorkspace\JavaLab\bin\cateage java Item  -->X  
+    				return:Error  
+    		  C:\java>java -cp C:\iii\java\javaWorkspace\JavaLab\bin TestPackage
+    				return:TestPackage's main:Asus notebook  
+    		                			
+*/	
+		/*	
+	jar cmd執行--->C:\java>java -jar Hello.jar
+    				return:Item's main 方法	
+			      C:\java>java -cp Hello.jar TestPackage
+    				return:TestPackage's main:Asus notebook		
+		 */	
 	}
 
 }
