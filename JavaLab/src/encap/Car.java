616d472894071cb4c07a343264fc61a3cf8eecf0
WuZhen-liang <a0978262695@gmail.com>
2021-05-11 14:35:40 +0800

    encap updata & Constructor

--- JavaLab/src/encap/Car.java
+++ JavaLab/src/encap/Car.java
@@ -1,20 +1,48 @@
 package encap;
 
-public class Car {
+import java.awt.Color;
 
-	private String model;
-	private int price;
-	
-	public void setModel(String model) {
-//		if ( m.equals("Prius") || m.equals("Yaris")) {
-//		}
-//		System.out.println("error");
-		this.model = model;
+import com.sun.jndi.url.iiopname.iiopnameURLContextFactory;
+
+public class Car {
+//下方程式為建構方法:Constructor
+	public Car(char c) {  //初始化預設內容
+		color = c;
 	}
+	public Car(String model,char color) {  //初始化預設內容
+//		this.model = model;
+		setModel(model);
+		this.color = color;
+		}
+//========================================	
+	private String model;  //預設值:null
+	private int price;     //預設值: 0
+	private char color;    //預設值:''
+	
+	
 	public String getModel() {
 		return model;
 	}
-	public int getPrice() {
-		return price;
+	public void setModel(String model) {
+		this.model = model;
+		if (model.equals("Yaris")) {
+			this.price = 575000;
+		}
+		else if (model.equals("Prius")){
+			this.price = 109000;
+		}
+		else {
+			System.out.println("Error:型號錯誤");
+		}
 	}
+	public int getPrice() {
+		return price; //lambda Car.this.price
+	}
+	public char getColar() {
+		return color;
+	}
+	public void setColar(char colar) {
+		this.color = colar;
+	}
+	
 }
--- JavaLab/src/encap/TestCar.java
+++ JavaLab/src/encap/TestCar.java
@@ -4,12 +4,14 @@ public class TestCar {
 
 	public static void main(String[] args) {
 //      encapsulation封裝 		
-		Car c = new Car();
+		Car c = new Car("Yaris",'Y'); //產生物件
 //		c.model = "Prius";
-		c.setModel("Prius");
+//		c.setModel("Prius");
+//		c.setModel("Yaris");
+		
 //		c.price = 1000000;
 		
-		System.out.printf("型號:%s,價錢:%7d",c.getModel(),c.getPrice());
+		System.out.printf("型號:%s,價錢:%7d,顏色:%c",c.getModel(),c.getPrice(),c.getColar());
 
 	}
 
