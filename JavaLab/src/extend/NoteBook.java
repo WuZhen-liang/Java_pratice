31b9404ef244fcecf856a60f0d71758232656df2
WuZhen-liang <a0978262695@gmail.com>
2021-05-13 10:05:00 +0800

    extand update

--- JavaLab/src/extend/NoteBook.java
+++ JavaLab/src/extend/NoteBook.java
@@ -6,19 +6,24 @@ public class NoteBook extends Product{
 //	public int price;
 	public int warranty;
 	
-	public NoteBook(String store,String name, int price, int warranty) {
-//		this.name = name;  //若父類別屬性改成private無法使用this
-//		this.price = price; //宣告　protected：子列別＆同個package 可以看到屬性
-		setName(name);
-		setPrice(price);
-		setStore(store);
+	public NoteBook(String name, int price, int warranty) {
+		super();//compiler會自動加一個在這裡
+//		super(name,price);
+		this.name = name;  //若父類別屬性改成private無法使用this
+		this.price = price; //宣告　protected：子列別＆同個package 可以看到屬性
+//		setName(name);
+//		setPrice(price);
+
 		this.warranty = warranty;
 	}
 //-----------Override:重新改寫父類別的方法-----------------	
 	//annotation
 	@Override
 		public String desc() {
-		return String.format("名稱：%s,價錢:%d,保固:%d天",name,price,warranty);
+		String info = super.desc();//可以呼叫父類別上的方法  //名稱:%s,價錢:%d
+//		return String.format("名稱：%s,價錢:%d,保固:%d天",name,price,warranty);
+		System.out.println(info);
+		return String.format("%s,!保固:%d天",info,warranty);
 		}
 	
 	public int getWarranty() {
--- JavaLab/src/extend/Product.java
+++ JavaLab/src/extend/Product.java
@@ -3,15 +3,12 @@ package extend;
 public class Product {
 //public:所有人,private:自己,protected:同個package&子類別 no modifier:必須為同一個package
 //	public String name;
-	protected String name;
-	protected int price;
-	public String store;
+	 String name;
+	 int price;
 	
-	public Product(String name, int price, String store) {
+	public Product(String name, int price) {
 		this.name = name;
-		this.price = price;
-		this.store = store;
-		
+		this.price = price;	
 	}
 
 	public Product() {
@@ -32,16 +29,10 @@ public class Product {
 	public void setPrice(int price) {
 		this.price = price;
 	}
-	public String desc() {
+	 public String desc() {
 		return String.format("名稱：%s,價錢:%d", name,price);
 	}
-	public void setStore(String store) {
-		this.store = store;
-		
-	}
-	public String getStore() {
-		return store;
-	}
+
 	
 	
 }
