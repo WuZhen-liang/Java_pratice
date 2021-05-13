9a05cd221066a6309e1931c20eb1fdd10606b3e6
WuZhen-liang <a0978262695@gmail.com>
2021-05-13 15:22:35 +0800

    updata SimCard

--- JavaLab/src/interFace/SimCard.java
+++ JavaLab/src/interFace/SimCard.java
@@ -2,8 +2,16 @@ package interFace;
 
 import java.util.Date;
 
-public class SimCard extends Product {
-	
+public class SimCard extends Product implements Warrantable,Expirable{
+//SimCard :SimCard,Product,Warrantable,Expirable 四種型態
+	@Override
+	public Date 最後期限() {
+		return expireDate;
+	}
+	@Override
+	public int 保固天數() {
+		return 7;
+	}
 	private Date expireDate;
 	
 	public SimCard(String name, int price, Date expireDate) {
