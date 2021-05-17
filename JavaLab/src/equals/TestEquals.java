55d30ac444b16c91f3842f08d0f3c176e408f0f3
WuZhen-liang <a0978262695@gmail.com>
2021-05-17 21:26:07 +0800

    update home practice

--- JavaLab/src/equals/TestEquals.java
+++ JavaLab/src/equals/TestEquals.java
@@ -4,7 +4,7 @@ public class TestEquals {
 
 	public static void main(String[] args) {
 		
-		Employee e1 = new Employee(null);
+		Employee e1 = new Employee("001");
 		Employee e2 = new Employee("001");
 		
 		System.out.println(e1 == e2);//compare memory location //false
