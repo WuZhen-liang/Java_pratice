42b49278b62d68fa649b5558b45aec801e6c9401
WuZhen-liang <a0978262695@gmail.com>
2021-05-20 23:24:43 +0800

    0520 home practice

--- JavaLab/src/io/CsvFileRead.java
+++ JavaLab/src/io/CsvFileRead.java
@@ -6,11 +6,17 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
-public class CsvFileRead {
+import com.sun.jmx.interceptor.DefaultMBeanServerInterceptor;
+
+import equals.Employee;
+
+public class CsvFileRead  {
+
 
 	public static void main(String[] args) {
-		
+
 		
 		File file = new File("C:\\java\\Employees.csv");
 		try (FileInputStream fis = new FileInputStream(file);
@@ -18,14 +24,26 @@ public class CsvFileRead {
 				BufferedReader br = new BufferedReader(isr);
 				){
 			String line;
-			int sum ;
+			int salary_sum = 0 ;
 			while((line = br.readLine())!= null) {
-				System.out.println(line);
+//				System.out.println(line);
 				String [] column = line.split(",");
-				System.out.println(column[2]);
-
+//				System.out.println(column[2]);
+				Scanner scanner = new Scanner(column[2]);
+				String answer = scanner.nextLine();
+//				System.out.println(answer);
+				if (answer.equals("薪水")) {
+					continue;
+				}
+				else {
+					int selection = Integer.parseInt(answer);
+					System.out.println(selection);					
+					salary_sum = salary_sum+selection;
+				}
  			}
-		} catch (IOException e) {
+			System.out.println("salary:"+salary_sum);
+		} 
+		catch (IOException e) {
 			e.printStackTrace();
 		}
 
